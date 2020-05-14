eeef4503c1cc4857fdcd5246679fe16ba3cdfb74
Kevin Hau <kevin.hau.official@gmail.com>
2020-05-14 02:15:11 -0400

    Bomb Trace
    Implemented a proper trace for bombs after they explode that detects players or objects hit by the bomb. Replaced wall BSPs with static meshes.

--- BudgetBomberman/Source/BudgetBomberman/Private/Bomb.cpp
+++ BudgetBomberman/Source/BudgetBomberman/Private/Bomb.cpp
@@ -4,6 +4,7 @@
 #include "Bomb.h"
 #include "Components/PrimitiveComponent.h"
 #include "DrawDebugHelpers.h"
+#include "Kismet/KismetSystemLibrary.h"
 #include "Math/Color.h"
 #include "UObject/ConstructorHelpers.h"
 #include "Bomberman.h"
@@ -37,7 +38,6 @@ void ABomb::Tick(float DeltaTime)
 
 	if (TimeSinceSpawned < ExplosionDelay)
 	{
-		
 		float BombRange = BombRangeBase + (BombRangeMultiplier - 1.f) * 100.f;
 
 		DrawDebugLine(
@@ -71,10 +71,87 @@ void ABomb::Tick(float DeltaTime)
 			FColor::Green,
 			false
 		);
+
+		
 	}
 	
 	else
 	{
+		float BombRange = BombRangeBase + (BombRangeMultiplier - 1.f) * 100.f;
+
+		TArray<AActor*> ActorsToIgnore;
+		FHitResult OutHit;
+
+		this->SetActorEnableCollision(false);
+
+		// Check West
+		UKismetSystemLibrary::BoxTraceSingle(
+			GetWorld(), 
+			FVector(GetActorLocation().X, GetActorLocation().Y, GetActorLocation().Z),
+			FVector(GetActorLocation().X, GetActorLocation().Y - BombRange, GetActorLocation().Z),
+			FVector(40.f, 0.f, 40.f),
+			FRotator(0.f, 0.f, 0.f),
+			ETraceTypeQuery::TraceTypeQuery_MAX,
+			false,
+			ActorsToIgnore,
+			EDrawDebugTrace::ForDuration,
+			OutHit,
+			true,
+			FLinearColor(FColor::Red),
+			FLinearColor(FColor::Green)
+		);
+
+		// Check North
+		UKismetSystemLibrary::BoxTraceSingle(
+			GetWorld(), 
+			FVector(GetActorLocation().X, GetActorLocation().Y, GetActorLocation().Z),
+			FVector(GetActorLocation().X + BombRange, GetActorLocation().Y, GetActorLocation().Z),
+			FVector(0.f, 40.f, 40.f),
+			FRotator(0.f, 0.f, 0.f),
+			ETraceTypeQuery::TraceTypeQuery_MAX,
+			false,
+			ActorsToIgnore,
+			EDrawDebugTrace::ForDuration,
+			OutHit,
+			true,
+			FLinearColor(FColor::Red),
+			FLinearColor(FColor::Green)
+		);
+
+		// Check East
+		UKismetSystemLibrary::BoxTraceSingle(
+			GetWorld(), 
+			FVector(GetActorLocation().X, GetActorLocation().Y, GetActorLocation().Z),
+			FVector(GetActorLocation().X, GetActorLocation().Y + BombRange, GetActorLocation().Z),
+			FVector(40.f, 0.f, 40.f),
+			FRotator(0.f, 0.f, 0.f),
+			ETraceTypeQuery::TraceTypeQuery_MAX,
+			false,
+			ActorsToIgnore,
+			EDrawDebugTrace::ForDuration,
+			OutHit,
+			true,
+			FLinearColor(FColor::Red),
+			FLinearColor(FColor::Green)
+		);
+
+		// Check South
+		UKismetSystemLibrary::BoxTraceSingle(
+			GetWorld(), 
+			FVector(GetActorLocation().X, GetActorLocation().Y, GetActorLocation().Z),
+			FVector(GetActorLocation().X - BombRange, GetActorLocation().Y, GetActorLocation().Z),
+			FVector(0.f, 40.f, 40.f),
+			FRotator(0.f, 0.f, 0.f),
+			ETraceTypeQuery::TraceTypeQuery_MAX,
+			false,
+			ActorsToIgnore,
+			EDrawDebugTrace::ForDuration,
+			OutHit,
+			true,
+			FLinearColor(FColor::Red),
+			FLinearColor(FColor::Green)
+		);
+
 		Destroy();
 	}
 	
---
+++ BudgetBomberman/Source/BudgetBomberman/Public/ArenaLevel.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/LevelScriptActor.h"
+#include "ArenaLevel.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUDGETBOMBERMAN_API AArenaLevel : public ALevelScriptActor
+{
+	GENERATED_BODY()
+	
+};
